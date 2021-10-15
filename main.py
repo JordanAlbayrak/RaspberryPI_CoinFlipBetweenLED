e12d62591de895ae875a4bd0845476baf169dba1
Jordan Albayrak <70965032+JordanAlbayrak@users.noreply.github.com>
2021-10-14 21:49:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
-from random import seed
-from random import random
-import RPi.GPIO as GPIO
-from time import sleep
+from random import randrange
+import RPi.GPIO as GPIO 
+from time import sleep 
 
-seed(1)
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BCM) 
 GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# while True:
-GPIO.output(4, GPIO.HIGH)
-GPIO.output(5, GPIO.HIGH)
-GPIO.output(6, GPIO.HIGH)
-sleep(1)
+
+
 GPIO.output(4, GPIO.LOW)
 GPIO.output(5, GPIO.LOW)
-GPIO.output(6, GPIO.LOW)
 sleep(1)
-counter = 0
+counter = 1
+y = False
 while True:
-
-    while GPIO.input(2) == GPIO.LOW:
-        if counter == 0:
+    if GPIO.input(2) == GPIO.LOW:
+        y = True
+        x = 0.5
+        i = 0
+        counter = 1
+        
+        
+    if y == True and counter == 1:   
+        if i <= x:
             GPIO.output(4, GPIO.HIGH)
             GPIO.output(5, GPIO.LOW)
-            GPIO.output(6, GPIO.LOW)
-            sleep(0.3)
-            counter += 1
-        elif counter == 1:
+            sleep(i)
             GPIO.output(4, GPIO.LOW)
             GPIO.output(5, GPIO.HIGH)
-            GPIO.output(6, GPIO.LOW)
-            sleep(0.3)
-            counter += 1
-        elif counter == 2:
-            GPIO.output(4, GPIO.LOW)
-            GPIO.output(5, GPIO.LOW)
-            GPIO.output(6, GPIO.HIGH)
-            sleep(0.3)
-            counter += 1
-        elif counter == 3:
-            GPIO.output(4, GPIO.LOW)
-            GPIO.output(5, GPIO.HIGH)
-            GPIO.output(6, GPIO.LOW)
-            sleep(0.3)
+            sleep(i)
+            print (i)
+            i += 0.05
+        elif i >= x:
+            ran = randrange(2)
+            print("RAN: " + str(ran))
+            if ran == 0:
+                GPIO.output(4, GPIO.LOW)
+                GPIO.output(5, GPIO.HIGH)
+            elif ran == 1:
+                GPIO.output(4, GPIO.HIGH)
+                GPIO.output(5, GPIO.LOW)
             counter = 0
-
-
